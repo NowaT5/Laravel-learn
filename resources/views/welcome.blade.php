cff885f74eebd56f962b5e2ee25dbfe2f46982f0
NowaT5 <nowa2536@gmail.com>
2024-01-07 16:05:09 +0700

    read more change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
     @foreach ($blogs as $item)
         <h2>{{$item->title}}</h2>
         <p>{!!Str::limit($item->content,50)!!}</p>
-        <a href="/detail/{{$item->id}}">อ่านเพิ่มเติม</a>
+        <a href="/detail/{{$item->id}}">อ่านต่อ</a>
         <hr>
     @endforeach
 @endsection
